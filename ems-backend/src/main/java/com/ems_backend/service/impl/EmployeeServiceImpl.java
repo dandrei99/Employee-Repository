10f3d4ba74c8de4df81940ec6a159addebd428d9
Dan Onofrei <dandrei.onofrei@gmail.com>
2024-09-28 14:35:57 +0300

    update Employee

--- ems-backend/src/main/java/com/ems_backend/service/impl/EmployeeServiceImpl.java
+++ ems-backend/src/main/java/com/ems_backend/service/impl/EmployeeServiceImpl.java
@@ -42,4 +42,19 @@ public class EmployeeServiceImpl implements EmployeeService {
        return employees.stream().map((employee) -> EmployeeMapper.mapToEmployeeDto(employee))
                .collect(Collectors.toList());
     }
+
+    @Override
+    public EmployeeDto updateEmployee(Long employeeId, EmployeeDto updatedEmployee) {
+        Employee employee = employeeRepository.findById(employeeId)
+                .orElseThrow(() ->
+                        new ResourceNotFoundExcepton("Employee does not exist with given id: "+ employeeId));
+
+        employee.setFirstName(updatedEmployee.getFirstName());
+        employee.setLastName(updatedEmployee.getLastName());
+        employee.setEmail(updatedEmployee.getEmail());
+
+       Employee updatedEmployeeObj = employeeRepository.save(employee);
+
+       return EmployeeMapper.mapToEmployeeDto(updatedEmployeeObj);
+    }
 }
